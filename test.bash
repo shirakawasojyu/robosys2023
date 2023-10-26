99a17e308051af104a0e0a07743f79cd77b94bfb
Sojyu Shirakawa <sojyu358@icloud.com>
2023-10-26 12:55:59 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Shirakwa Sojyu
+# SPDX-Licsnse-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at line $1
+	res=1
+}
 
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
