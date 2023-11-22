16a1c9d767d6ef3ecd3c322cd68f3b50deb94b85
Sojyu Shirakawa <sojyu358@icloud.com>
2023-11-22 19:18:19 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -13,6 +13,18 @@ res=0
 out=$(seq 5 | ./plus)
 
 [ "${out}" = 15 ] || ng ${LINENO}
+#[ "${out}" = 15 ] || ng ${LINENO}
+
+
+### STRAGE INPUT ###
+#out=$(echo あ | ./plus)
+#[ "$?" = 1 ]      || ng ${LINENO}
+#[ "${out}" = "" ] || ng ${LINENO}
+
+#out=$(echo | ./plus)
+#[ "$?" = 1 ]      || ng ${LINENO}
+#[ "${out}" = "" ] || ng ${LINENO}
+
 
 [ "$res" = 0 ] && echo OK
 exit $res
